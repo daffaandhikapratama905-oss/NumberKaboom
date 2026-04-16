e4eed932485db871975889f18fde5c560984a759
Daffa Andhika Pratama <daffaandhikapratama905@gmail.com>
2026-04-17 00:14:14 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'minesweeper-app',
+  webDir: 'www'
+};
+
+export default config;
